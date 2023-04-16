fa8d6977e2d912837cee348aa9e6c5c764418ea6
Gustavo Carvalho <gustavo.csemail@gmail.com>
2023-04-16 19:20:25 -0300

    Adjust layouts and themes

--- app/src/main/java/com/example/coisos/login/view/LoginActivity.kt
+++ app/src/main/java/com/example/coisos/login/view/LoginActivity.kt
@@ -3,19 +3,32 @@ package com.example.coisos.login.view
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.coisos.R
 import com.example.coisos.databinding.ActivityLoginBinding
-import com.example.coisos.view.MainActivity
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
 
+    companion object {
+        fun startLoginActivity(context: Context) {
+            val intent = Intent(context, LoginActivity::class.java)
+            context.startActivity(intent)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        val buttonRegisterActivity = findViewById<TextView>(R.id.text_view_create_account)
+
+        buttonRegisterActivity.setOnClickListener {
+            RegisterActivity.startRegisterActivity(this)
+        }
     }
 }
--- app/src/main/java/com/example/coisos/login/view/RegisterActivity.kt
+++ app/src/main/java/com/example/coisos/login/view/RegisterActivity.kt
@@ -1,16 +1,33 @@
 package com.example.coisos.login.view
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.coisos.R
 import com.example.coisos.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityRegisterBinding
 
+    companion object {
+        fun startRegisterActivity(context: Context) {
+            val intent = Intent(context, RegisterActivity::class.java)
+            context.startActivity(intent)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val buttonRegisterActivity = findViewById<TextView>(R.id.text_view_login_account)
+
+        buttonRegisterActivity.setOnClickListener {
+            LoginActivity.startLoginActivity(this)
+        }
     }
 }
