0f680153f4a4e2b06cc7c66aae65375c317636e0
Gustavo Carvalho <gustavo.csemail@gmail.com>
2023-06-02 13:20:35 -0300

    Add hilt

---
+++ app/src/main/java/com/example/coisos/CoisosApp.kt
@@ -0,0 +1,13 @@
+package com.example.coisos
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CoisosApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
